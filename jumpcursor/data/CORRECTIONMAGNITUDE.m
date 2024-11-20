28c13631f5f95446d7fe88beb89bfa28a285397c
vbot-lab <No Email>
2024-11-20 13:06:47 +0100

    pp09-13

--- jumpcursor/data/CORRECTIONMAGNITUDE.m
+++ jumpcursor/data/CORRECTIONMAGNITUDE.m
@@ -1,4 +1,4 @@
-wl = load('pp07.mat');
+wl = load('participant8.mat');
 num_trials = size(wl.TimeStamp, 1);
 correctionX = NaN(1, num_trials);  % Store signed correction in the x-direction
 jumpDistances = NaN(1, num_trials);
